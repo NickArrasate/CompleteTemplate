74b1ef8d333ce5ac50f361d511157aacda526c59
connor cummings and nick arrasate <connor@>
2016-05-26 14:37:10 -0700

    add never button. retrieves address info from geocode. repairs marker location

--- js/map-interface.js
+++ js/map-interface.js
@@ -4,19 +4,23 @@ maps.KEY = require("./../.env").mapapiKey;
 function initMap(x,y) {
   maps.load(function(google) {
 
-    var myLatLng = {lat: 45.5145, lng: -122.6801};
+    var myLatLng = {lat: x, lng: y};
     var markLatLng = {lat: x,
                       lng: y
                     };
     var map = new google.maps.Map(document.getElementById('map_canvas'), {
-      zoom: 10,
+      zoom: 4,
       center: myLatLng,
       mapTypeId: google.maps.MapTypeId.SATELLITE
+
     });
+
     var marker = new google.maps.Marker({
       position: markLatLng,
       map: map,
-      title: 'Hello World!'
+      content: 'Hello World!'
     });
+    console.log(marker);
+    console.log(map);
   });
 }
